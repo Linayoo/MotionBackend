649d15beef270f61e1663449fe415d659bafca7d
Miri Yoo <violinmiriu@gmail.com>
2022-10-19 15:12:44 +0200

    #5 endpoint get the user's followers added

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -6,3 +6,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = ["id", "last_login", "is_superuser", "is_staff", "is_active", "date_joined", "created", "updated"]
+
+
+class TestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['followers']
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from users.views import LoggedInUserProfileView
+from users.views import LoggedInUserProfileView, ListLoggedInUserFollowers
 
 urlpatterns = [
     path("api/users/me/", LoggedInUserProfileView.as_view()),
+    path("api/social/followers/followers/", ListLoggedInUserFollowers.as_view())
 ]
