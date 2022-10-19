8f4077137ecb0b646e0f98e9bd6b8e24ac6d977a
Miri Yoo <violinmiriu@gmail.com>
2022-10-19 13:02:46 +0200

    token url

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,4 +3,5 @@ from users.views import LoggedInUserProfileView, UpdateLoggedInUserProfile
 
 urlpatterns = [
     path("api/users/me/", LoggedInUserProfileView.as_view()),
+    path("api/users/me/", UpdateLoggedInUserProfile.as_view()),
 ]
