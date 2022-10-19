0a6499e2fcbfaa94bf63d04cdb686c75a58f6dbf
Miri Yoo <violinmiriu@gmail.com>
2022-10-19 10:43:35 +0200

    To check if my superuser is created, added view to look at all the users Conda update added to Dockerfile

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from users.views import LoggedInUserProfileView
+from users.views import LoggedInUserProfileView, GetAllUsers
 
 urlpatterns = [
     path("api/users/me/", LoggedInUserProfileView.as_view()),
+    path("api/users/", GetAllUsers.as_view()),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,11 +1,21 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
+
 from users.models import User
 from users.serializers import UserSerializer
 
 
 class LoggedInUserProfileView(ListAPIView):
-    serializer = UserSerializer
+    serializer_class = UserSerializer
 
     def get_queryset(self):
         queryset = User.objects.filter(id=self.request.user)
         return queryset
+
+
+class GetAllUsers(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
