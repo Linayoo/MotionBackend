8db64074c5b8652750623d8a63155334e8a0ebb5
Miri Yoo <violinmiriu@gmail.com>
2022-10-19 15:18:59 +0200

    #6 endpoint get the user's followees added

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from users.views import LoggedInUserProfileView, ListLoggedInUserFollowers
+from users.views import LoggedInUserProfileView, ListLoggedInUserFollowers, ListLoggedInUserFollowees
 
 urlpatterns = [
     path("api/users/me/", LoggedInUserProfileView.as_view()),
-    path("api/social/followers/followers/", ListLoggedInUserFollowers.as_view())
+    path("api/social/followers/followers/", ListLoggedInUserFollowers.as_view()),
+    path("api/social/followers/following/", ListLoggedInUserFollowees.as_view()),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,6 @@
 from rest_framework.generics import RetrieveUpdateAPIView, get_object_or_404, RetrieveAPIView
 from users.models import User
-from users.serializers import UserSerializer, TestSerializer
+from users.serializers import UserSerializer, RetrieveFollowerSerializer, RetrieveFolloweeSerializer
 
 
 # GET & PATCH: /api/users/me/
@@ -21,7 +21,23 @@ class LoggedInUserProfileView(RetrieveUpdateAPIView):
 # api/social/followers/followers/
 # GET: List of all the logged-in user’s followers
 class ListLoggedInUserFollowers(RetrieveAPIView):
-    serializer_class = TestSerializer
+    serializer_class = RetrieveFollowerSerializer
+    queryset = User.objects.all()
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset, id=self.request.user.id)
+        return obj
+
+    def get_queryset(self):
+        queryset = User.objects.all()
+        return queryset
+
+
+# api/social/followers/following/
+# GET: List of all the people the current logged-in user is following
+class ListLoggedInUserFollowees(RetrieveAPIView):
+    serializer_class = RetrieveFolloweeSerializer
     queryset = User.objects.all()
 
     def get_object(self):
